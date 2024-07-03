b8dde2616120d7af403e6c9d130e03a6884f21dd
Christoph Jeworutzki <christoph@digital-loop.de>
2024-07-03 15:46:51 +0200

    chore: update config files + add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
